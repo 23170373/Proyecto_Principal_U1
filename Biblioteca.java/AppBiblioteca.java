ff70880a007072aea13b2a76869bed47a813b90a
23170373 <158121527+23170373@users.noreply.github.com>
2025-09-22 15:40:31 +0000

    Version 1.0

---
+++ Biblioteca.java/AppBiblioteca.java
@@ -0,0 +1,9 @@
+package Biblioteca;
+
+public class AppBiblioteca {
+    
+   public static void main(String[] args) {
+        Principal ventana = new Principal("Ejemplo de Biblioteca");
+        ventana.setVisible(true);
+   }
+}
---
+++ Biblioteca.java/Principal.java
@@ -0,0 +1,29 @@
+//Sistema de Control Bibliotecario
+
+/*  Objetivo:
+ *  Desarrollar una aplicación que permita gestionar libros, usuarios y préstamos, con una interfaz
+ *  gráfica amigable y funcional.
+ *  
+ *  Requisitos técnicos:
+ *  
+ *  • Lenguaje: Java
+ *  • Componentes GUI: JFrame, JPanel, JButton, JTextField, JTable, JComboBox, etc.
+ *  • Persistencia: Archivos planos o serializable.
+ *  • Validaciones: Entrada de datos, campos obligatorios, formatos.
+ * 
+ *  Funcionalidades minimas:
+ * 
+ *  1. Pantalla principal con menú de navegación.
+ *  2. Registro de libros (alta, baja, modificación).
+ *  3. Registro de usuarios.
+ *  4. Prestamo y devolución de libros.
+ *  5. Visulización de datos en tablas.
+ *  6. Mensajes de confirmación y error.|
+ */
+
+
+package Biblioteca;
+
+public class Principal {
+
+}
